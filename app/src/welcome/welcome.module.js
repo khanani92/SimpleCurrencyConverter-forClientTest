74c591002d975f47c1d1c9f6160bbcf6bad2c7c3
khanani92 <muddassir_92@hotmail.com>
2016-03-19 00:38:56 +0500

    'page_title'

--- app/src/welcome/welcome.module.js
+++ app/src/welcome/welcome.module.js
@@ -19,7 +19,7 @@
         url:'/welcome',
         templateUrl:'src/welcome/welcome.html',
         controller: 'Welcome as vm',
-        title: 'ng-Super Welcome'
+        title: 'Currency Converter '
       });
   }
 
